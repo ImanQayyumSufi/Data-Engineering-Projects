ad5592f75195615ab0185e80ff5d673ae921240a
Iman Qayyum <miqsufi2@gmail.com>
2024-02-07 22:14:51 +0800

    code started

---
+++ calculator.py
@@ -0,0 +1,6 @@
+# This file contains all the calculations
+
+def get_monthly_interest_rate():
+    principal = float(input("Enter principal amount: "))
+    interest_rate = float(input("Enter interest rate (%): "))
+    duration = int(input("Enter loan duration: "))
